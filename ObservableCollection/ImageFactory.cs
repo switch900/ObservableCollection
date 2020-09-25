64925cd5f1f1497b85dc7266a0dc54d4b76b0a8d
Andrew <35283726+switch900@users.noreply.github.com>
2020-09-24 20:00:30 -0700

    Add project files.

---
+++ ObservableCollection/ImageFactory.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ObservableCollection
+{
+    class ImageFactory
+    {
+        public static int count = 0;
+        private static readonly Images _myactualdata = new Images();
+        public static Images MyImages
+        {
+            get
+            {
+                return _myactualdata;
+            }
+        }
+        public class Images : ObservableCollection<ImageData>
+        {
+            public Images()
+            {
+                this.Add(new ImageData(@"Resources\Images\1.jpg"));
+                this.Add(new ImageData(@"Resources\Images\2.jpg"));
+                this.Add(new ImageData(@"Resources\Images\3.jpg"));
+            }
+        }
+
+        public class ImageData : INotifyPropertyChanged
+        {
+            private String _sImageName = "";
+            public ImageData(string sImageName)
+            {
+                ImageFactory.count++;
+                ImageName = sImageName;
+            }
+
+            public String ImageName
+            {
+                get
+                {
+                    return _sImageName;
+                }
+                set
+                {
+                    _sImageName = value;
+                    NotifyPropertyChanged(ImageName);
+                }
+            }
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected void NotifyPropertyChanged(String sProp)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(sProp));
+
+            }
+        }
+    }
+}
---
+++ ObservableCollection/Resources/ResourceDictionary.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ObservableCollection.Resources
+{
+    /// <summary>
+    /// Interaction logic for ResourceDictionary.xaml
+    /// </summary>
+    public partial class ResourceDictionary
+    {
+        public ResourceDictionary()
+        {
+            InitializeComponent();
+        }
+    }
+}
